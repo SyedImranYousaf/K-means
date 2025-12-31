cab0b35de2e498b3cc3ac8a8b9960bdae3b6becd
Amish ali <syedimranpk836@gmail.com>
2025-12-31 20:52:28 +0500

    path set

--- task2/app.py
+++ task2/app.py
@@ -5,7 +5,7 @@ from sklearn.preprocessing import StandardScaler
 
 st.title("Customer Clustering App")
 
-data = pd.read_csv("Wholesale_customers_data.csv")
+data = pd.read_csv("task2/Wholesale_customers_data.csv")
 
 # data = pd.read_csv("C:\\Users\\Hp\\OneDrive\\Desktop\\AI_5th_semester\\lab13kmeans\\K-means\\task2\\Wholesale_customers_data.csv")
 
@@ -15,11 +15,11 @@ X_scaled = scaler.fit_transform(data)
 algo = st.selectbox("Select Algorithm", ["K-Means", "DBSCAN"])
 
 if algo == "K-Means":
-    model = pickle.load(open("kmeans_model.pkl", "rb"))
+    model = pickle.load(open("task2/kmeans_model.pkl", "rb"))
     # model = pickle.load(open("C:\\Users\\Hp\\OneDrive\\Desktop\\AI_5th_semester\\lab13kmeans\\K-means\\task2\\kmeans_model.pkl", "rb"))
     labels = model.predict(X_scaled)
 else:
-    model = pickle.load(open("dbscan_model.pkl", "rb"))
+    model = pickle.load(open("task2/dbscan_model.pkl", "rb"))
         # model = pickle.load(open("C:\\Users\\Hp\\OneDrive\\Desktop\\AI_5th_semester\\lab13kmeans\\K-means\\task2\\dbscan_model.pkl", "rb"))
     labels = model.fit_predict(X_scaled)
 
